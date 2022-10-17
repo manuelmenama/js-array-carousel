50e0dd3b52850c5cdb2137f31f7c3de44b8cd8b4
manuelmenama <manuel.mena090589@gmail.com>
2022-10-17 17:49:22 +0200

    ended mil2

--- milestone-1/js/script.js
+++ milestone-1/js/script.js
@@ -1,1 +1,26 @@
-console.log('carosello');
+const imgArray = [
+  '01.jpg',
+  '02.jpg',
+  '03.jpg',
+  '04.jpg',
+  '05.jpg'
+];
+
+let imgStamp = '';
+
+const imgContainer = document.querySelector('.img-container');
+
+for(let i = 0; i < imgArray.length; i++){
+  imgStamp += `<img class="img-displayed" src="img/${imgArray[i]}" alt="${imgArray[i]}">`;
+  console.log(imgStamp);
+}
+
+
+imgContainer.innerHTML += imgStamp;
+
+let counterImg = 0;
+
+const imgDisplayed = document.getElementsByClassName('img-displayed');
+
+imgDisplayed[counterImg].classList.add('active');
+
